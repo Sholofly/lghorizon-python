9eaa2c6ddd91d1159eda8773021b6cb1f1a234c3
Rudolf Offereins <r.offereins@gmail.com>
2022-10-15 22:20:38 +0000

    :broom: Update init

--- lghorizon/__init__.py
+++ lghorizon/__init__.py
@@ -1,7 +1,5 @@
 """Python client for LG Horizon."""
-from .lghorizon_api import (
-    LGHorizonApi, 
-    LGHorizonBox, 
-    LGHorizonApiUnauthorizedError,
-    LGHorizonApiConnectionError) # noqa
+from .lghorizon_api import LGHorizonApi
+from .models.lghorizon_box import LGHorizonBox
+from .models.exceptions import LGHorizonApiUnauthorizedError, LGHorizonApiConnectionError
 from .const import ONLINE_RUNNING, ONLINE_STANDBY # noqa
