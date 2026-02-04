b1543eaa204d5be2d200e9d346a62245838a417e
Rudolf Offereins <r.offereins@gmail.com>
2026-02-04 06:25:27 +0000

    Fix position and duration

--- lghorizon/lghorizon_device_state_processor.py
+++ lghorizon/lghorizon_device_state_processor.py
@@ -154,13 +154,11 @@ class LGHorizonDeviceStateProcessor:
         device_state.season_number = replay_event.season_number
         device_state.episode_number = replay_event.episode_number
         device_state.show_title = replay_event.title
-        now_in_ms = int(time.time() * 1000)
-
-        device_state.last_position_update = int(time.time() * 1000)
+        device_state.last_position_update = int(time.time())
         device_state.start_time = replay_event.start_time
         device_state.end_time = replay_event.end_time
         device_state.duration = replay_event.end_time - replay_event.start_time
-        device_state.position = now_in_ms - int(replay_event.start_time * 1000)
+        device_state.position = int(time.time()) - int(replay_event.start_time)
 
         # Add random number to url to force refresh
         join_param = "?"
@@ -200,8 +198,10 @@ class LGHorizonDeviceStateProcessor:
         device_state.season_number = replay_event.season_number
         device_state.episode_number = replay_event.episode_number
         device_state.show_title = replay_event.title
-        device_state.last_position_update = player_state.last_speed_change_time
-        device_state.position = player_state.relative_position
+        device_state.last_position_update = int(
+            player_state.last_speed_change_time / 1000
+        )
+        device_state.position = int(player_state.relative_position / 1000)
         device_state.start_time = replay_event.start_time
         device_state.end_time = replay_event.end_time
         device_state.duration = replay_event.end_time - replay_event.start_time
@@ -237,22 +237,19 @@ class LGHorizonDeviceStateProcessor:
         device_state.id = replay_event.event_id
         # Iets met buffer doen
         channel = self._channels[replay_event.channel_id]
-        padding = channel.replay_pre_padding + channel.replay_post_padding
         device_state.source_type = source.source_type
         device_state.channel_id = channel.id
         device_state.episode_title = replay_event.episode_name
         device_state.season_number = replay_event.season_number
         device_state.episode_number = replay_event.episode_number
         device_state.show_title = replay_event.title
-        device_state.last_position_update = int(time.time() * 1000)
+        device_state.last_position_update = int(
+            player_state.last_speed_change_time / 1000
+        )
         device_state.start_time = replay_event.start_time
         device_state.end_time = replay_event.end_time
-        device_state.duration = (
-            replay_event.end_time - replay_event.start_time + padding
-        )
-        device_state.position = (
-            player_state.relative_position + channel.replay_pre_padding
-        )
+        device_state.duration = replay_event.end_time - replay_event.start_time
+        device_state.position = int(player_state.relative_position / 1000)
         # Add random number to url to force refresh
         device_state.image = await self._get_intent_image_url(replay_event.event_id)
 
@@ -286,8 +283,8 @@ class LGHorizonDeviceStateProcessor:
             device_state.show_title = vod.title
 
         device_state.duration = vod.duration
-        device_state.last_position_update = int(time.time() * 1000)
-        device_state.position = player_state.relative_position
+        device_state.last_position_update = int(time.time())
+        device_state.position = int(player_state.relative_position / 1000)
 
         device_state.image = await self._get_intent_image_url(vod.id)
 
@@ -317,8 +314,10 @@ class LGHorizonDeviceStateProcessor:
         device_state.episode_title = recording.episode_title
         device_state.season_number = recording.season_number
         device_state.episode_number = recording.episode_number
-        device_state.last_position_update = player_state.last_speed_change_time
-        device_state.position = player_state.relative_position
+        device_state.last_position_update = int(
+            player_state.last_speed_change_time / 1000
+        )
+        device_state.position = int(player_state.relative_position / 1000)
         if recording.start_time:
             device_state.start_time = int(
                 dt.fromisoformat(
